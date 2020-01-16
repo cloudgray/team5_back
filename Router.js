7578ecd07c5a9f675bfbf38f2ea4ac70f494bb5a
cloudgray <bluejet1104@gmail.com>
2020-01-16 10:35:15 +0900

    POST api/data 수정, 더미 데이터 수정

--- Router.js
+++ Router.js
@@ -9,6 +9,7 @@ Router.get("/data", (req, res) => {
 Router.post("/data", (req, res) => {
   const result = updateData(data);
   res.status(200).json(result);
+  console.log(data);
 });
 
 // 함수들
@@ -16,9 +17,9 @@ const updateData = data => {
   let index = Math.floor(Math.random() * 7);
   let justTest = Math.random() * 10 > 3 ? true : false;
   if (justTest) {
-    data[index.testCnt] += 1;
+    data[index].testCnt = data[index].testCnt + 1;
   } else {
-    data[index.purchaseCnt] += 1;
+    data[index].purchaseCnt = data[index].purchaseCnt + 1;
   }
 
   return data;
@@ -33,8 +34,8 @@ let data = [
     id: 1,
     name: "코듀로이베이지",
     color: "#C3574D",
-    testCnt: 184,
-    purchaseCnt: 80,
+    testCnt: 18,
+    purchaseCnt: 8,
     imgUrl:
       "http://image.ethefaceshop.com/tfsshopWebSrc/upload/product/201907/AF010043_06_2.jpg"
   },
@@ -42,8 +43,8 @@ let data = [
     id: 2,
     name: "브라운실루엣",
     color: "#A52021",
-    testCnt: 256,
-    purchaseCnt: 39,
+    testCnt: 25,
+    purchaseCnt: 3,
     imgUrl:
       "http://image.ethefaceshop.com/tfsshopWebSrc/upload/product/201907/AF010043_06_3.jpg"
   },
@@ -51,8 +52,8 @@ let data = [
     id: 3,
     name: "브릿지코랄",
     color: "#C63A39",
-    testCnt: 274,
-    purchaseCnt: 18,
+    testCnt: 27,
+    purchaseCnt: 1,
     imgUrl:
       "http://image.ethefaceshop.com/tfsshopWebSrc/upload/product/201907/AF010043_06_4.jpg"
   },
@@ -60,8 +61,8 @@ let data = [
     id: 4,
     name: "피치코랄",
     color: "#DF2336",
-    testCnt: 471,
-    purchaseCnt: 162,
+    testCnt: 47,
+    purchaseCnt: 16,
     imgUrl:
       "http://image.ethefaceshop.com/tfsshopWebSrc/upload/product/201907/AF010043_06_5.jpg"
   },
@@ -69,8 +70,8 @@ let data = [
     id: 5,
     name: "코랄아이콘",
     color: "#DF3F32",
-    testCnt: 483,
-    purchaseCnt: 75,
+    testCnt: 48,
+    purchaseCnt: 7,
     imgUrl:
       "http://image.ethefaceshop.com/tfsshopWebSrc/upload/product/201907/AF010043_06_6.jpg"
   },
@@ -78,8 +79,8 @@ let data = [
     id: 6,
     name: "페스티벌레드",
     color: "#D7000F",
-    testCnt: 493,
-    purchaseCnt: 36,
+    testCnt: 49,
+    purchaseCnt: 3,
     imgUrl:
       "http://image.ethefaceshop.com/tfsshopWebSrc/upload/product/201907/AF010043_06_7.jpg"
   },
@@ -87,8 +88,8 @@ let data = [
     id: 7,
     name: "레드파우치",
     color: "#9D0004",
-    testCnt: 284,
-    purchaseCnt: 35,
+    testCnt: 28,
+    purchaseCnt: 3,
     imgUrl:
       "http://image.ethefaceshop.com/tfsshopWebSrc/upload/product/201907/AF010043_06_8.jpg"
   }
